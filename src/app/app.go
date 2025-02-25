cf83d72cdc74fea5139c35a81e6501f83bdcfcbd
Thiago Pereira <titiura@gmail.com>
2025-02-24 21:39:41 -0300

    refactor: use gorilla/mux for routing

--- src/app/app.go
+++ src/app/app.go
@@ -2,17 +2,31 @@ package app
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func Start() {
 
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+	router := mux.NewRouter()
 
-	err := http.ListenAndServe("localhost:8000", nil)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{custormer_id:[0-9]+}", getCustomers).Methods(http.MethodGet)
+
+	err := http.ListenAndServe("localhost:8000", router)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w,  vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received")
 }
