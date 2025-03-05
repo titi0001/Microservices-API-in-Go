11b7451e1a3d4ad7fb99f581216294a6380c4f6f
Thiago Pereira <titiura@gmail.com>
2025-03-04 21:43:40 -0300

    refactor: refatora código em app.go para melhorar legibilidade

--- src/app/app.go
+++ src/app/app.go
@@ -3,23 +3,51 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"log"
 
 	"github.com/gorilla/mux"
-	"github.com/titi0001/Microservices-API-in-Go/src/service"
+	"github.com/joho/godotenv"
 	"github.com/titi0001/Microservices-API-in-Go/src/domain"
+	"github.com/titi0001/Microservices-API-in-Go/src/service"
 )
 
 func Start() {
 
-	router := mux.NewRouter()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	ch := CustomerHandler{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
+
+	router := mux.NewRouter()
+	repo := domain.NewCustomerRepositoryDb()
+
+	ch := CustomerHandler{service.NewCustomerService(repo)}
 
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 
-	err := http.ListenAndServe("localhost:8000", router)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
+	server := &http.Server{
+		Addr:    "localhost:8000",
+		Handler: router,
 	}
+
+	go func() {
+		fmt.Println("Server started on localhost:8000")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Error starting server:", err)
+		}
+	}()
+
+	// Aguardar sinal para fechar o servidor
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+
+	// Fechar o banco de dados
+	fmt.Println("Shutting down server...")
+	repo.Close()
 
 }
