8a65bc70d40ecc260e431a22121d036d609532aa
Thiago Pereira <titiura@gmail.com>
2025-03-06 21:04:56 -0300

    refactor(service): update interface to use AppError type

--- src/service/customerService.go
+++ src/service/customerService.go
@@ -1,21 +1,24 @@
 package service
 
-import "github.com/titi0001/Microservices-API-in-Go/src/domain"
+import (
+	"github.com/titi0001/Microservices-API-in-Go/src/domain"
+	"github.com/titi0001/Microservices-API-in-Go/src/errs"
+)
 
 type CustomerService interface {
-	GetAllCustomer() ([]domain.Customer, error)
-	GetCustomer(string) (*domain.Customer, error)
+	GetAllCustomer() ([]domain.Customer, *errs.AppError)
+	GetCustomer(string) (*domain.Customer, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer() ([]domain.Customer, error) {
+func (s DefaultCustomerService) GetAllCustomer() ([]domain.Customer, *errs.AppError) {
 	return s.repo.FindAll()
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, error) {
+func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
 	return s.repo.ById(id)
 }
 
