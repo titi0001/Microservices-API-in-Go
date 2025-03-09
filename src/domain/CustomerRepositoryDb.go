4bf3cce7f9f4a76729dfdc2ae8cb5490c2916850
Thiago Pereira <titiura@gmail.com>
2025-03-09 19:58:33 -0300

    feat: update FindAll to return dto.CustomerResponse. Modified FindAll query and return type

--- src/domain/CustomerRepositoryDb.go
+++ src/domain/CustomerRepositoryDb.go
@@ -8,58 +8,39 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/titi0001/Microservices-API-in-Go/src/errs"
 	"github.com/titi0001/Microservices-API-in-Go/src/logger"
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
-	var rows *sql.Rows
-	var err error
-
-	FindAllSql := "SELECT customer_id, name, date_of_birth, city, zipcode, status FROM customers"
-	rows, _ = d.client.Query(FindAllSql)
-	if status != "" {
-		FindAllSql := "SELECT customer_id, name, date_of_birth, city, zipcode, status FROM customers WHERE status = ?"
-		
-		rows, err = d.client.Query(FindAllSql, status)
-		if err != nil {
-			logger.Error("Error while querying customer table" + err.Error())
-			return nil, errs.NewUnexpectedError("Unexpected database error")
-		}
-	}
-	defer rows.Close()
-
 	customers := make([]Customer, 0)
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
+
+	if status != "" {
+		findAllSql := "SELECT customer_id, name, date_of_birth, city, zipcode, status FROM customers WHERE status = ?"
+		err := d.client.Select(&customers, findAllSql)
 		if err != nil {
 			logger.Error("Error while scanning customer" + err.Error())
-			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
-		customers = append(customers, c)
+	} else {
+		findAllSql := "SELECT customer_id, name, date_of_birth, city, zipcode, status FROM customers WHERE status = ?"
+		err := d.client.Select(&customers, findAllSql, status)
+		if err != nil {
+			logger.Error("Error while scanning customer" + err.Error())
+		}
 	}
-
 	return customers, nil
-}
-
-func (d *CustomerRepositoryDb) Close() {
-	if d.client != nil {
-		d.client.Close()
-	}
 }
 
 func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "SELECT customer_id, name, date_of_birth, city, zipcode, status FROM customers WHERE customer_id = ?"
-	row := d.client.QueryRow(customerSql, id)
 
 	var c Customer
-
-	err := row.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
+	err := d.client.Get(&c, customerSql, id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
@@ -80,23 +61,23 @@ func NewCustomerRepositoryDb() CustomerRepositoryDb {
 	if user == "" || password == "" || dbName == "" {
 		log.Fatal("Missing required environment variables: MYSQL_USER, MYSQL_PASSWORD, or MYSQL_DATABASE")
 	}
-
 	// string de conexão .env
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
 
-	client, err := sql.Open("mysql", dsn)
+	client, err := sqlx.Open("mysql", dsn)
 	if err != nil {
-		panic(err.Error())
-	}
-
-	// Testa a conexão com o banco
-	if err := client.Ping(); err != nil {
-		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+		logger.Error("Error connecting to database" + err.Error())
 	}
 
 	client.SetConnMaxLifetime(time.Minute * 3)
 	client.SetMaxOpenConns(10)
 	client.SetMaxIdleConns(10)
 
-	return CustomerRepositoryDb{client: client}
+	return CustomerRepositoryDb{client}
+}
+
+func (d *CustomerRepositoryDb) Close() {
+	if d.client != nil {
+		d.client.Close()
+	}
 }
