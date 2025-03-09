6cf397091f0d8b9084e8f60ef0c638eefb7e8330
Thiago Pereira <titiura@gmail.com>
2025-03-09 17:28:17 -0300

    Feat: create NewCustomerService receive to  params repo domain.CustomerRepository and return defaultCustomerService to fix app.go file and  add conditions to GetAllCustomers recipe status

--- src/service/customerService.go
+++ src/service/customerService.go
@@ -5,23 +5,33 @@ import (
 	"github.com/titi0001/Microservices-API-in-Go/src/errs"
 )
 
+func NewCustomerService(repo domain.CustomerRepository) CustomerService {
+    return DefaultCustomerService{repo: repo}
+}
+
+
 type CustomerService interface {
-	GetAllCustomer() ([]domain.Customer, *errs.AppError)
 	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetAllCustomer(status string) ([]domain.Customer, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer() ([]domain.Customer, *errs.AppError) {
-	return s.repo.FindAll()
+func (s DefaultCustomerService) GetAllCustomer(status string ) ([]domain.Customer, *errs.AppError) {
+	if status == "active" {
+		status = "1"
+	} else if status == "inactive" {
+		status = "0"
+	} else {	
+		status = ""
+	}
+	return s.repo.FindAll(status)
 }
+
 
 func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
 	return s.repo.ById(id)
 }
 
-func NewCustomerService(repo domain.CustomerRepository) DefaultCustomerService {
-	return DefaultCustomerService{repo}
-}
