d343a3136c17d48fbb1f309b9992de9fe2888b4f
Thiago Pereira <titiura@gmail.com>
2025-03-09 19:57:49 -0300

    feat: update GetAllCustomers to return dto.CustomerResponse. Adjusted interface and DefaultCustomerService logic

--- src/service/customerService.go
+++ src/service/customerService.go
@@ -2,36 +2,48 @@ package service
 
 import (
 	"github.com/titi0001/Microservices-API-in-Go/src/domain"
+	"github.com/titi0001/Microservices-API-in-Go/src/dto"
 	"github.com/titi0001/Microservices-API-in-Go/src/errs"
 )
 
-func NewCustomerService(repo domain.CustomerRepository) CustomerService {
-    return DefaultCustomerService{repo: repo}
-}
-
-
 type CustomerService interface {
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
-	GetAllCustomer(status string) ([]domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
+	GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer(status string ) ([]domain.Customer, *errs.AppError) {
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
 		status = "0"
-	} else {	
+	} else {
 		status = ""
 	}
-	return s.repo.FindAll(status)
+	customers, err := s.repo.FindAll(status)
+	if err != nil {
+		return nil, err
+	}
+	response := make([]dto.CustomerResponse, 0)
+	for _, c := range customers {
+		response = append(response, c.ToDto())
+	}
+	return response, err
 }
 
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+	response := c.ToDto()
+	return &response, nil
 }
 
+func NewCustomerService(repo domain.CustomerRepository) CustomerService {
+	return DefaultCustomerService{repo: repo}
+}
