b224c9ef4e1ec706b9b7b1774c15d79c817d9783
Thiago Pereira <titiura@gmail.com>
2025-03-10 21:24:24 -0300

    refactor(domain): func newCustomerRepositoryDb

--- src/domain/CustomerRepositoryDb.go
+++ src/domain/CustomerRepositoryDb.go
@@ -2,10 +2,7 @@ package domain
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"os"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -51,29 +48,8 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	return &c, nil
 }
 
-func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
-	dbName := os.Getenv("MYSQL_DATABASE")
-	host := "localhost"
-	port := "3306"
-
-	if user == "" || password == "" || dbName == "" {
-		log.Fatal("Missing required environment variables: MYSQL_USER, MYSQL_PASSWORD, or MYSQL_DATABASE")
-	}
-	// string de conexão .env
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
-
-	client, err := sqlx.Open("mysql", dsn)
-	if err != nil {
-		logger.Error("Error connecting to database" + err.Error())
-	}
-
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-
-	return CustomerRepositoryDb{client}
+func NewCustomerRepositoryDb(dbClient *sqlx.DB) CustomerRepositoryDb {
+	return CustomerRepositoryDb{dbClient}
 }
 
 func (d *CustomerRepositoryDb) Close() {
