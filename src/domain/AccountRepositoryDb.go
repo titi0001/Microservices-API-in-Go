786a1ed2c65ee3b6ac433f8fc5dbfea09bb8d5b4
Thiago Pereira <titiura@gmail.com>
2025-03-11 22:06:59 -0300

    feat(domain): implement transaction save in AccountRepositoryDB

--- src/domain/AccountRepositoryDb.go
+++ src/domain/AccountRepositoryDb.go
@@ -21,8 +21,8 @@ func (d AccountRepositoryDb) Save(a Account) (*Account, *errs.AppError) {
 		return nil, errs.NewUnexpectedError("Unexpect error from database")
 	}
 
-	 id, err:=result.LastInsertId()
-	 if err != nil {
+	id, err := result.LastInsertId()
+	if err != nil {
 		logger.Error("Error while getting last insert id for new account: " + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpect error from database")
 	}
@@ -32,7 +32,65 @@ func (d AccountRepositoryDb) Save(a Account) (*Account, *errs.AppError) {
 
 }
 
-func NewAccountRepositoryDb(dbClient *sqlx.DB) AccountRepository{
-	return AccountRepositoryDb{dbClient}
+func (d AccountRepositoryDb) SaveTransaction(t Transaction) (*Transaction, *errs.AppError) {
+	tx, err := d.client.Begin()
+	if err != nil {
+		logger.Error("Error while starting a new transaction for bank account transaction: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
+
+	result, _ := tx.Exec(`INSET INTO transactions (account_id, amount, transaction_type, transaction_date)
+						values (?, ?, ?, ?)`, t.AccountId, t.Amount, t.TransactionType, t.TransactionDate)
+
+	if t.IsWithdrawal() {
+		_, err = tx.Exec(`UPDATE accounts SET amount = amount - ? WHERE account_id = ?`, t.Amount, t.AccountId)
+		if err != nil {
+			return nil, errs.NewUnexpectedError("Failed to update account for withdrawal: " + err.Error())
+		}
+	} else {
+		_, err = tx.Exec(`UPDATE accounts SET amount = amount + ? WHERE account_id = ?`, t.Amount, t.AccountId)
+		if err != nil {
+			return nil , errs.NewUnexpectedError("Failed to update account for deposit: " + err.Error())
+		}
+	}
+
+	if err := tx.Rollback(); err != nil {
+		logger.Error("Error while saving transaction: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
+
+	if err = tx.Commit(); err != nil {
+		logger.Error("Error while commiting transaction for bank account: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
+
+	transactionId, err := result.LastInsertId()
+	if err != nil {
+		logger.Error("Error while getting transaction id: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
+
+	account, appErr := d.FindBy(t.AccountId)
+	if appErr != nil {
+		return nil, appErr
+	}
+	t.TransactionId = strconv.FormatInt(transactionId, 10)
+
+	t.Amount = account.Amount
+	return &t, nil
 }
 
+func (d AccountRepositoryDb) FindBy(accountId string) (*Account, *errs.AppError) {
+	sqlGetAccount := "SELECT account_id, opening_date, account_type, amount FROM accounts WHERE account_id = ?"
+	var account Account
+	err := d.client.Get(&account, sqlGetAccount, accountId)
+	if err != nil {
+		logger.Error("Error while fetching account information: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
+	return &account, nil
+}
+
+func NewAccountRepositoryDb(dbClient *sqlx.DB) AccountRepository {
+	return AccountRepositoryDb{dbClient}
+}
