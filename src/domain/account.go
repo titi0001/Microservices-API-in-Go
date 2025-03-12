bba5c3775edfb7286c4748d7a7725ebfe0507a5d
Thiago Pereira <titiura@gmail.com>
2025-03-11 22:06:41 -0300

    feat(domain): add transaction model and account withdrawal logic

--- src/domain/account.go
+++ src/domain/account.go
@@ -5,6 +5,8 @@ import (
 	"github.com/titi0001/Microservices-API-in-Go/src/errs"
 )
 
+const dbTSLayout = "2006-01-02 15:04:05"
+
 type Account struct {
 	AccountId   string
 	CustomerId  string
@@ -14,10 +16,27 @@ type Account struct {
 	Status      string
 }
 
-func (a Account) ToNewAccountResponseDto() dto.NewAccountResponse {
-	return dto.NewAccountResponse{AccountId: a.AccountId}
+func (a Account) ToNewAccountResponseDto() *dto.NewAccountResponse {
+	return &dto.NewAccountResponse{AccountId: a.AccountId}
 }
 
 type AccountRepository interface {
 	Save(Account) (*Account, *errs.AppError)
+	SaveTransaction(transaction Transaction) (*Transaction, *errs.AppError)
+	FindBy(accountId string) (*Account, *errs.AppError)
+}
+
+func (a Account) CanWithdraw(amount float64) bool {
+	return a.Amount < amount
+
+}
+
+func NewAccount(customerId, accountType string, amount float64) Account {
+	return Account{
+		CustomerId:  customerId,
+		OpeningDate: dbTSLayout,
+		AccountType: accountType,
+		Amount:      amount,
+		Status:      "1",
+	}
 }
