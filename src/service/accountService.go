5cc813470b75d336e1b60b07a4cb44048c7b8d36
Thiago Pereira <titiura@gmail.com>
2025-03-11 22:07:17 -0300

    feat(service): add MakeTransaction in account service

--- src/service/accountService.go
+++ src/service/accountService.go
@@ -7,8 +7,11 @@ import (
 	"time"
 )
 
+const dbTSLayout = "2006-01-02 15:04:05"
+
 type AccountService interface {
-	NewAccount(dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError)
+	NewAccount(request dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError)
+	MakeTransaction(request dto.TransactionRequest) (*dto.TransactionResponse, *errs.AppError)
 }
 
 type DefaultAccountService struct {
@@ -20,27 +23,48 @@ func (s DefaultAccountService) NewAccount(req dto.NewAccountRequest) (*dto.NewAc
 	if err != nil {
 		return nil, err
 	}
-	
-	a := domain.Account{
-		AccountId:   "",
-		CustomerId:  req.CustomerId,
-		OpeningDate: time.Now().Format("2006-01-02 15:04:05"),
-		AccountType: req.AccountType,
-		Amount:      req.Amount,
-		Status:      "1",
-	}
 
-	newAccount, err := s.repo.Save(a)
+	account := domain.NewAccount(req.CustomerId, req.AccountType, req.Amount)
+	if newAccount, err := s.repo.Save(account); err != nil {
+		return nil, err
+	} else {
+		return newAccount.ToNewAccountResponseDto(), nil
+	}
+}
+
+func (s DefaultAccountService) MakeTransaction(req dto.TransactionRequest) (*dto.TransactionResponse, *errs.AppError) {
+
+	err := req.Validate()
 	if err != nil {
 		return nil, err
 	}
 
-	response := newAccount.ToNewAccountResponseDto()
+	if req.IsTransactionTypeWithdrawal() {
+		account, err := s.repo.FindBy(req.AccountId)
+		if err != nil {
+			return nil, err
+		}
+		if !account.CanWithdraw(req.Amount) {
+			return nil, errs.NewValidationError("Insufficient balance in the account")
+		}
+	}
 
+	t := domain.Transaction{
+		AccountId:       req.AccountId,
+		Amount:          req.Amount,
+		TransactionType: req.TransactionType,
+		TransactionDate: time.Now().Format(dbTSLayout),
+	}
+
+	transaction, appError := s.repo.SaveTransaction(t)
+	if appError != nil {
+		return nil, appError
+	}
+	response := transaction.ToDto()
 	return &response, nil
 
 }
 
-func NewAccountService(repo  domain.AccountRepository) DefaultAccountService {
+func NewAccountService(repo domain.AccountRepository) DefaultAccountService {
 	return DefaultAccountService{repo}
 }
