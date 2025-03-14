e2d4d13f0a311ae0e6489c1b2d1eb2cf4600f561
Thiago Pereira <titiura@gmail.com>
2025-03-13 21:31:02 -0300

    feat(auth): add AuthMiddleware for route authorization

---
+++ src/app/authMiddleware.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/titi0001/Microservices-API-in-Go/src/domain"
+	"github.com/titi0001/Microservices-API-in-Go/src/errs"
+)
+
+type AuthMiddleware struct {
+	repo domain.AuthRepository
+}
+
+func (a AuthMiddleware) authorizationHandler() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			currentRoute := mux.CurrentRoute(r)
+			currentRouteVars := mux.Vars(r)
+			authHeader := r.Header.Get("Authorization")
+
+			if authHeader != "" {
+				token := getTokenFromHeader(authHeader)
+
+				isAuthorized := a.repo.IsAuthorized(token, currentRoute.GetName(), currentRouteVars)
+				if isAuthorized {
+					next.ServeHTTP(w, r)
+				} else {
+					appError := errs.AppError{Code: http.StatusForbidden, Message: "Unauthorized"}
+					writeResponse(w, appError.Code, appError.AsMessage())
+				}
+			} else {
+				writeResponse(w, http.StatusUnauthorized, "missing token")
+			}
+
+		})
+	}
+}
+
+func getTokenFromHeader(header string) string {
+	splitToken := strings.Split(header, "Bearer")
+	if len(splitToken) == 2 {
+		return strings.TrimSpace(splitToken[1])
+	}
+	return ""
+}
