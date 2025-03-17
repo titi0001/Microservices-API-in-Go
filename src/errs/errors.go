86666be1a322237290cc4e0c3361c997d23c95e8
Thiago Pereira <titiura@gmail.com>
2025-03-16 21:41:13 -0300

    feat(errors): improve error handling

--- src/errs/errors.go
+++ src/errs/errors.go
@@ -1,10 +1,16 @@
 package errs
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type AppError struct {
-	Code    int  `json:",omitempty"`
+	Code    int    `json:",omitempty"`
 	Message string `json:"message"`
+}
+
+func (e *AppError) Error() string {
+	return e.Message
 }
 
 func (e AppError) AsMessage() *AppError {
@@ -30,6 +36,41 @@ func NewUnexpectedError(message string) *AppError {
 func NewValidationError(message string) *AppError {
 	return &AppError{
 		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
+
+func NewBadRequestError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusBadRequest,
+		Message: message,
+	}
+}
+
+func NewForbiddenError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusForbidden,
+		Message: message,
+	}
+}
+
+func NewConflictError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusConflict,
+		Message: message,
+	}
+}
+
+func NewTooManyRequestsError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusTooManyRequests,
 		Message: message,
 	}
 }
