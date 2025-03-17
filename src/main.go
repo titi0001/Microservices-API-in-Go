d10b298908d102974701041d0ae600e3ceac87c6
Thiago Pereira <titiura@gmail.com>
2025-03-16 21:47:46 -0300

    Refactor(main): remove logger to main

--- src/main.go
+++ src/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/titi0001/Microservices-API-in-Go/src/app"
-	"github.com/titi0001/Microservices-API-in-Go/src/logger"
 )
 
 func main() {
 
-	logger.Info("Starting the application...")
 	app.Start()
 }
