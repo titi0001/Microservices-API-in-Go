0ab4a9dd439450e78a8a048a00633ceedb938724
Thiago Pereira <titiura@gmail.com>
2025-03-17 20:00:03 -0300

    fix(domain): update AuthService interface to return *RolePermissions and remove User struct

--- src/domain/auth.go
+++ src/domain/auth.go
@@ -1,19 +1,10 @@
 package domain
 
 import (
-	"database/sql"
 	"io"
 
 	"github.com/titi0001/Microservices-API-in-Go/src/errs"
 )
-
-type User struct {
-	Username   string        `db:"username"`
-	Password   string        `db:"password"`
-	Role       string        `db:"role"`
-	CustomerId sql.NullInt64 `db:"customer_id"`
-	CreatedOn  string        `db:"created_on"`
-}
 
 type AuthRepository interface {
 	FindUser(username, password string) (*User, *errs.AppError)
@@ -23,7 +14,6 @@ type AuthRepository interface {
 type AuthService interface {
 	RemoteLogin(body io.Reader) ([]byte, *errs.AppError)
 	RemoteIsAuthorized(token string, routeName string, vars map[string]string) (bool, *errs.AppError)
-	GetRolePermissions() RolePermissions
-	GetSecretKey() []byte 
+	GetRolePermissions() *RolePermissions
+	GetSecretKey() []byte
 }
-
