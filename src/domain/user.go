caa6efa7f0d480be53f5cced764c9961acbb35a3
Thiago Pereira <titiura@gmail.com>
2025-03-19 21:37:16 -0300

    lint(domain): User and UserDetails domain

--- src/domain/user.go
+++ src/domain/user.go
@@ -4,18 +4,17 @@ import "database/sql"
 
 type User struct {
 	Username   string        `db:"username"`
+	CustomerId sql.NullInt64 `db:"customer_id"`
 	Password   string        `db:"password"`
 	Role       string        `db:"role"`
-	CustomerId sql.NullInt64 `db:"customer_id"`
 	CreatedOn  string        `db:"created_on"`
 }
 
 type UserDetails struct {
 	Username string
 	UserID   string
-
-	Role  string
-	Roles []string
+	Role     string
+	Roles    []string
 
 	CustomClaims map[string]interface{}
 }
