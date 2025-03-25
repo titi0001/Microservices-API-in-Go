c0e48e9303f316aa683411bd0fa893d195e1fa08
Thiago Pereira <titiura@gmail.com>
2025-03-24 23:00:11 -0300

    feat(errs): add custom error handling with AppError

---
+++ errs/errors.go
@@ -0,0 +1,76 @@
+package errs
+
+import (
+	"net/http"
+)
+
+type AppError struct {
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message"`
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+
+func (e *AppError) AsMessage() string {
+	return e.Message
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
+
+func NewBadRequestError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusBadRequest,
+		Message: message,
+	}
+}
+
+func NewForbiddenError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusForbidden,
+		Message: message,
+	}
+}
+
+func NewConflictError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusConflict,
+		Message: message,
+	}
+}
+
+
+func NewTooManyRequestsError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusTooManyRequests,
+		Message: message,
+	}
+}
