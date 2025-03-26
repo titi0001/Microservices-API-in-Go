7d33674e94f5dd64a2ee5b82cd75f8bd1b6a8f3e
Thiago Pereira <titiura@gmail.com>
2025-03-25 22:37:56 -0300

    feat: enhance refresh token flow with token deletion

--- domain/ports/auth_repository.go
+++ domain/ports/auth_repository.go
@@ -5,9 +5,10 @@ import (
 	"github.com/titi0001/Microservices-API-in-Go/errs"
 )
 
-
 type AuthRepository interface {
 	FindUser(username, password string) (*domain.User, *errs.AppError)
 	VerifyPermission(role string, customerId string, routeName string, vars map[string]string) bool
-	SaveUser(user domain.User) (*domain.User, *errs.AppError) 
+	SaveUser(user domain.User) (*domain.User, *errs.AppError)
+	SaveRefreshToken(refreshToken string) *errs.AppError
+	VerifyRefreshToken(refreshToken string) (bool, *errs.AppError)
 }
